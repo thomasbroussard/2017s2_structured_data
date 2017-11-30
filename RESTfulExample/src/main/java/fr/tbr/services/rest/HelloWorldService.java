8149ea81386721b6fe46db7cee1412b15fcdbb33
tbrou <tbrou@tbrou-PC7.mshome.net>
2017-11-30 20:28:17 +0100

    * package refactor

--- RESTfulExample/src/main/java/fr/tbr/services/rest/HelloWorldService.java
+++ RESTfulExample/src/main/java/fr/tbr/services/rest/HelloWorldService.java
@@ -1,7 +1,6 @@
 package fr.tbr.services.rest;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -19,14 +18,5 @@ public class HelloWorldService {
 
 	}
 
-	@POST
-	@Path("/{param}")
-	public Response createMessage(@PathParam("param") String msg) {
-
-		final String output = "Jersey say : " + msg;
-
-		return Response.status(200).entity(output).build();
-
-	}
 
 }
